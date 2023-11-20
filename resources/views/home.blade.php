18b30e6630c0b1a69aa8a63bf1d6a5df1fc9110c
MrArcano <salvatore-dario@hotmail.it>
2023-11-20 14:05:33 +0100

    [add]: PageController + Movie Model

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,17 @@
 
 @section('content')
     <h1>Home</h1>
+    <div class="d-flex flex-wrap">
+        @foreach ($movies as $movie)
+            <div class="card text-bg-dark m-3" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $movie->title }}</h5>
+                    <h6 class="card-title">Original title: {{ $movie->original_title }}</h6>
+                    <p class="card-text">Nationality: {{ $movie->nationality }}</p>
+                    <p class="card-text">{{ $movie->date }}</p>
+                    <p class="card-text">Vote: {{ $movie->vote }}</p>
+                </div>
+            </div>
+        @endforeach
+    </div>
 @endsection
